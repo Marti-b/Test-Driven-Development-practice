5430061d927cc45e6cafc0f445b2eea892419e79
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-09-21 11:32:59 +0200

    Fibonacci Numbers
    Using Trangulation

--- Test Driven Development practice/Fibonacci Numbers.cs
+++ Test Driven Development practice/Fibonacci Numbers.cs
@@ -3,4 +3,27 @@ using NUnit.Framework;
 namespace Tests
 {
 
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        public void TestFibonacci(int expected, int index)
+        {
+            Assert.AreEqual(expected, GetFibonacci(index));
+
+        }
+
+        private int GetFibonacci(int index)
+        {
+            if (index == 0)
+                return 0;
+            if (index == 1)
+                return 1;
+
+            return GetFibonacci(index - 1) + GetFibonacci(index - 2);
+        }
+    }
 }
