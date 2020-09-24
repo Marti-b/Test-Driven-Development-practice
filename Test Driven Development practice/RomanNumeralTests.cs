9e6f5b1ba1825b554510fd0106f9baadfe373eee
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-09-24 14:31:03 +0200

    Roman Numerals
    Returning intigers from roman numerals

---
+++ Test Driven Development practice/RomanNumeralTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestFixture]
+    class RomanNumeralTests
+    {
+        [TestCase(1, "I")]
+        [TestCase(5, "V")]
+        [TestCase(10, "X")]
+        [TestCase(2, "II")]
+        [TestCase(4, "IV")]
+        [TestCase(14, "XIV")]
+        [TestCase(9, "IX")]
+        public void Parse(int expected, string roman)
+        {
+            Assert.AreEqual(expected, Roman.Parse(roman));
+        }
+    }
+    public class Roman
+    {
+        private static Dictionary<char, int> map = new Dictionary<char, int>()
+        {
+            {'I', 1 },
+            {'V', 5 },
+            {'X', 10 },
+            {'L', 50 },
+            {'C', 100 },
+            {'D', 500 },
+            {'M', 1000 }
+        };
+        public static int Parse(string roman)
+        {
+            int result = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && map[roman[i]] < map[roman[i + 1]])
+                {
+                    result -= map[roman[i]];
+                }
+                else
+                {
+                    result += map[roman[i]];
+                }
+               
+            }
+            return result;
+
+        }
+    }
+
+}
